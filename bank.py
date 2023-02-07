a81fe38dab83cc38419ed151ddf6fd4e3efd76e7
pc-hm20 <pcallaghan@homecaremedical.ie>
2023-02-07 20:06:09 +0000

    Adding Task 2 submission

---
+++ bank.py
@@ -0,0 +1,20 @@
+import decimal
+import math
+
+first_coins = input("How much money (in cent) are you depositing first? - ")
+second_coins = input("How much money (in cent) are you depositing second? - ")
+
+total1 = int(first_coins) / 100
+total2 = int(second_coins) / 100 
+
+rounded_total1 = decimal.Decimal(total1).quantize(decimal.Decimal('0.00'))
+rounded_total2 = decimal.Decimal(total2).quantize(decimal.Decimal('0.00'))
+
+balance = rounded_total1 + rounded_total2
+if balance > 1:
+    print(f"Your balance is €{balance}. Don't spend it all in one shop.")
+else: 
+    print(f"Your balance is €{balance}. It won't even get you a bag of crisps these days.")
+
+#Reference
+#https://www.tutorialspoint.com/How-to-round-down-to-2-decimals-a-float-using-Python#:~:text=Use%20the%20ceil()%20function,and%20print%20the%20resultant%20number.
